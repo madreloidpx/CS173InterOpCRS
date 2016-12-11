c87a96714488b281ec22752e424d626014c05ac9
jpsangabriel <jprenzsangabriel@gmail.com>
2016-12-11 23:05:26 +0800

    Add getter functions

--- server.php
+++ server.php
@@ -1,17 +1,91 @@
 <?php
-	function getUserInfo($symbol){}
+	ini_set('display_errors','Off');
+	function getUserInfo($userID, $type){
+		$conn=mysqli_connect('localhost','root','');
+		switch($type){
+			case 'student':
+				$query = "SELECT lastname, firstname, sex, contact, email, address from students.students WHERE students.students.id='$userID'";;
+			break;
+			case 'staff':
+				$query = "SELECT lastname, firstname, sex, contact, email, address from faculty_and_admin.staff WHERE faculty_and_admin.staff.id='$userID'";;
+			break;
+			default:
+				$query=NULL;
+			break;
+		}
+		$result = mysqli_query($conn,$query);
+		$solutions = array();
+		while($row = mysqli_fetch_assoc($result)) {
+		  $solutions[] = $row;
+		}
+		return $solutions;
+	}
 	
-	function getStudentInfo($symbol){}
+	function getStudentInfo($userID){
+		$conn=mysqli_connect('localhost','root','');
+		$query = "SELECT lastname, firstname, sex, contact, email, address, student_number, bracket, academic_status, enrollment_status from students.students WHERE students.students.id='$userID'";
+		$result = mysqli_query($conn,$query);
+		$solutions = array();
+		while($row = mysqli_fetch_assoc($result)) {
+		  $solutions[] = $row;
+		}
+		return $solutions;
+	}
 	
-	function getAcademicStatus($symbol){}
+	function getAcademicStatus($userID){
+		$conn=mysqli_connect('localhost','root','');
+		$query = "SELECT academic_status from students.students WHERE students.students.id='$userID'";
+		$result = mysqli_query($conn,$query);
+		$solutions = array();
+		while($row = mysqli_fetch_assoc($result)) {
+		  $solutions[] = $row;
+		}
+		return $solutions;
+	}
 	
-	function getEnrollmentStatus($symbol){}
+	function getEnrollmentStatus($userID){
+		$conn=mysqli_connect('localhost','root','');
+		$query = "SELECT enrollment_status from students.students WHERE students.students.id='$userID'";
+		$result = mysqli_query($conn,$query);
+		$solutions = array();
+		while($row = mysqli_fetch_assoc($result)) {
+		  $solutions[] = $row;
+		}
+		return $solutions;
+	}
 	
-	function getStudentBracket($symbol){}
+	function getStudentBracket($userID){
+		$conn=mysqli_connect('localhost','root','');
+		$query = "SELECT bracket from students.students WHERE students.students.id='$userID'";
+		$result = mysqli_query($conn,$query);
+		$solutions = array();
+		while($row = mysqli_fetch_assoc($result)) {
+		  $solutions[] = $row;
+		}
+		return $solutions;
+	}
 	
-	function getAnnouncment($symbol){}
+	function getAnnouncement($symbol){
+		$conn=mysqli_connect('localhost','root','');
+		$query = "SELECT * from students.students WHERE firstname='Hitsugi'";
+		$result = mysqli_query($conn,$query);
+		$solutions = array();
+		while($row = mysqli_fetch_assoc($result)) {
+		  $solutions[] = $row;
+		}
+		return $solutions;
+	}
 	
-	function getGrades($symbol){}
+	function getGrades($userID, $courseID){
+		$conn=mysqli_connect('localhost','root','');
+		$query = "SELECT title, status  from students.grades JOIN courses.courses ON students.grades.course_id=courses.courses.id WHERE students.grades.student_id=$userID and students.grades.course_id=$courseID";
+		$result = mysqli_query($conn,$query);
+		$solutions = array();
+		while($row = mysqli_fetch_assoc($result)) {
+		  $solutions[] = $row;
+		}
+		return $solutions;
+	}
 	
 	function setInfo($username, $type, $lastname, $firstname, $email, $sex, $address, $contact){
 		$conn = mysql_pconnect("localhost", "root", "");
@@ -165,6 +239,23 @@
 	$server = new soap_server();
 	$server->configureWSDL('crsServer', 'urn:server');
 	
+	$server->wsdl->addComplexType('students_array','complexType','struct','all','',array('id'=>array('name'=>'id','type'=>'xsd:int'),'username'=>array('name'=>'username','type'=>'xsd:string'), 'password'=>array('name'=>'password','type'=>'xsd:string'), 'lastname'=>array('name'=>'lastname','type'=>'xsd:string'), 'firstname'=>array('name'=>'firstname','type'=>'xsd:string'), 'email'=>array('name'=>'email','type'=>'xsd:string'), 'sex'=>array('name'=>'sex','type'=>'xsd:string'), 'address'=>array('name'=>'address','type'=>'xsd:string'), 'contact'=>array('name'=>'contact','type'=>'xsd:int'), 'academic_status'=>array('name'=>'academic_status','type'=>'xsd:unsignedByte'), 'bracket'=>array('name'=>'bracket','type'=>'xsd:string'), 'student_number'=>array('name'=>'student_number','type'=>'xsd:string'), 'enrollment_status'=>array('name'=>'enrollment_status','type'=>'xsd:unsignedByte'), 'approval_status'=>array('name'=>'approval_status','type'=>'xsd:unsignedByte')));
+	$server->wsdl->addComplexType('students_array_php','complexType','array','all','SOAP-ENC:Array',array(),array(array('ref'=>'SOAP-ENC:arrayType','wsdl:arrayType'=>'tns:students_array[]')),'tns:students_array');
+
+	$server->wsdl->addComplexType('grades_array','complexType','struct','all','',array('id'=>array('name'=>'id','type'=>'xsd:int'), 'student_id'=>array('name'=>'student_id','type'=>'xsd:int'), 'course_id'=>array('name'=>'course_id','type'=>'xsd:int'), 'status'=>array('name'=>'status','type'=>'xsd:unsignedByte'), 'title'=>array('name'=>'title','type'=>'xsd:string')));
+	$server->wsdl->addComplexType('grades_array_php','complexType','array','all','SOAP-ENC:Array',array(),array(array('ref'=>'SOAP-ENC:arrayType','wsdl:arrayType'=>'tns:grades_array[]')),'tns:grades_array');
+
+
+	$server->wsdl->addComplexType('announcements_array','complexType','struct','all','',array('id'=>array('name'=>'id','type'=>'xsd:int'),'author_id'=>array('name'=>'author_id','type'=>'xsd:int'),'title'=>array('name'=>'title','type'=>'xsd:string'),'announcement_level'=>array('name'=>'announcement_level','type'=>'xsd:unsignedByte'),'content'=>array('name'=>'content','type'=>'xsd:string'),'date_created'=>array('name'=>'date_created','type'=>'xsd:date')));
+	$server->wsdl->addComplexType('announcements_array_php','complexType','array','all','SOAP-ENC:Array',array(),array(array('ref'=>'SOAP-ENC:arrayType','wsdl:arrayType'=>'tns:announcements_array[]')),'tns:announcements_array');
+		
+	$server->register("getUserInfo",array('userID' => 'xsd:int', 'type'=>'xsd:string'),array('return' => 'tns:students_array_php'),'urn:server','urn:server#getUserInfo');
+	$server->register("getStudentInfo",array('userID' => 'xsd:int'),array('return' => 'tns:students_array_php'),'urn:server','urn:server#getStudentInfo');
+	$server->register("getAcademicStatus",array('userID' => 'xsd:int'),array('return' => 'tns:students_array_php'),'urn:server','urn:server#getAcademicStatus');
+	$server->register("getEnrollmentStatus",array('userID' => 'xsd:int'),array('return' => 'tns:students_array_php'),'urn:server','urn:server#getEnrollmentStatus');
+	$server->register("getStudentBracket",array('userID' => 'xsd:int'),array('return' => 'tns:students_array_php'),'urn:server','urn:server#getStudentBracket');
+	$server->register("getAnnouncement",array(''),array('return' => 'tns:announcements_array_php'),'urn:server','urn:server#getAnnouncement');
+	$server->register("getGrades",array('userID' => 'xsd:int','courseID'=>'xsd:int'),array('return' => 'tns:grades_array_php'),'urn:server','urn:server#getGrades');
 	$server->register("setInfo", array('username' => 'xsd:string', 'type' => 'xsd:string', 'lastname' => 'xsd:string', 'firstname' => 'xsd:string', 'email' => 'xsd:string', 'sex' => 'xsd:string', 'address' => 'xsd:string', 'contact' => 'xsd:string'), array('return' => 'xsd:boolean'), 'urn:server', 'urn:server#setInfo');
 	$server->register("setStudentInfo", array('username' => 'xsd:string', 'bracket' => 'xsd:string', 'student_number' => 'xsd:string'), array('return' => 'xsd:boolean'), 'urn:server', 'urn:server#setStudentInfo');
 	$server->register("setAcademicStatus", array('username' => 'xsd:string', 'academic_status' => 'xsd:int'), array('return' => 'xsd:boolean'), 'urn:server', 'urn:server#setAcademicStatus');
